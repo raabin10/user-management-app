01a12ceca95b278d1495be2d65fd18e0f39743f6
raabin10 <raabinsinghasokkumar@gmail.com>
2024-10-02 07:48:11 +0530

    first

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,9 +6,9 @@ const Home = () => {
   return (
     <div>
       <h1 style={{ textAlign: 'center', padding: '20px' }}>Home</h1>
-      <Users />
+      <Users /> 
     </div>
   );
-};
+}; 
 
 export default Home;
